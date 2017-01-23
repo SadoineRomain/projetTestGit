d532b6b79325a1eeb925011df511cfcb824be260
DamienEsteve <m.damien.esteve@hotmail.fr>
2017-01-23 15:07:18 +0100

    Commit de merde

--- TestGitWeb/src/blabla/Bonsoir.java
+++ TestGitWeb/src/blabla/Bonsoir.java
@@ -2,7 +2,8 @@ package blabla;
 
 public class Bonsoir {
 	private String nom="Moi";
-
+	private Double gnagna=777D;
+	
 	public Bonsoir(String nom) {
 		this.nom = nom;
 		System.out.println("Mon nom est "+this.nom);
