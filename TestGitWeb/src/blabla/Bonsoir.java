6fe5944cded72873e32b79e2981bd9785e347525
DamienEsteve <m.damien.esteve@hotmail.fr>
2017-01-23 15:09:34 +0100

    Commit test conflit

--- TestGitWeb/src/blabla/Bonsoir.java
+++ TestGitWeb/src/blabla/Bonsoir.java
@@ -3,7 +3,7 @@ package blabla;
 public class Bonsoir {
 	private String nom="Moi";
 	private Double gnagna=777D;
-	
+	private double blabla=55;
 	public Bonsoir(String nom) {
 		this.nom = nom;
 		System.out.println("Mon nom est "+this.nom);
